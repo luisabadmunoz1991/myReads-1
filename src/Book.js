0a34c7bcc70e12fafc14319d0125808d1f73500f
Rich Haase <rdhaase@gmail.com>
2017-08-22 13:41:25 -0600

    type: feat
    list authors comma separated when more than 1 author exists

--- src/Book.js
+++ src/Book.js
@@ -36,7 +36,7 @@ class Book extends Component {
         </div>
         <div className="book-title">{this.props.title}</div>
         <div className="book-authors">{
-          Array.isArray(this.props.authors) ? this.props.authors[0] : this.props.authors
+          Array.isArray(this.props.authors) ? this.props.authors.join(', ') : this.props.authors
         }</div>
       </div>
     )
